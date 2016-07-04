9fce25b047233073f2b258d168639167f30fee87
谭帅 <jerry@danwi.me>
2016-07-04 17:56:02 +0800

    完善帮助类,获取应用程序版本号和名称

--- eq/src/main/java/com/example/tanshuai/easy_http/utils/TDevice.java
+++ eq/src/main/java/com/example/tanshuai/easy_http/utils/TDevice.java
@@ -2,7 +2,10 @@ package com.example.tanshuai.easy_http.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+
+import com.example.tanshuai.easy_http.EasyApplication;
 
 import java.io.File;
 
@@ -29,7 +32,43 @@ public class TDevice {
         context.startActivity(intent);
     }
 
-    public static void unInstallApk(Context context,File file){
-        
+    public static void unInstallApk(Context context, File file) {
+
+    }
+
+    /**
+     * 获取应用程序版本
+     *
+     * @param packageName
+     * @return
+     */
+    public static int getVersionCode(String packageName) {
+        int versionCode = 0;
+        try {
+            versionCode = EasyApplication.getContext().getPackageManager()
+                    .getPackageInfo(packageName, 0).versionCode;
+        } catch (PackageManager.NameNotFoundException ex) {
+            versionCode = 0;
+        }
+        return versionCode;
+    }
+
+    /**
+     * 获取应用名称
+     *
+     * @return
+     */
+    public static String getVersionName() {
+        String name = "";
+        try {
+            name = EasyApplication
+                    .getContext()
+                    .getPackageManager()
+                    .getPackageInfo(EasyApplication.getContext().getPackageName(),
+                            0).versionName;
+        } catch (PackageManager.NameNotFoundException ex) {
+            name = "";
+        }
+        return name;
     }
 }
