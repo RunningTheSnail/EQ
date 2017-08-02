60d9fa8ef3fcc422b6736753e8eb040395ffbbbf
jerry <jerry@danwi.me>
2017-08-02 14:30:45 +0800

    添加EQConfig配置类

--- eq/src/main/java/me/danwi/eq/EQApplication.java
+++ eq/src/main/java/me/danwi/eq/EQApplication.java
@@ -3,16 +3,9 @@ package me.danwi.eq;
 import android.app.Application;
 import android.content.Context;
 
-import java.io.File;
-import java.util.Collections;
-import java.util.List;
-
 import me.danwi.eq.core.ServiceProducers;
 import me.danwi.eq.third.ImgLoader;
 import me.danwi.eq.third.PicassoLoader;
-import me.danwi.eq.utils.FileUtils;
-import me.danwi.eq.utils.SdCardUtils;
-import okhttp3.Interceptor;
 
 /**
  * Created with Android Studio.
@@ -32,16 +25,25 @@ public abstract class EQApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
+        EQConfig eqConfig = new EQConfig() {
+            @Override
+            public boolean isDebug() {
+                return false;
+            }
+
+            @Override
+            public String getUrl() {
+                return null;
+            }
+        };
         new ServiceProducers.Builder()
-                .url(getUrl())
-                .connectTimeOut(connectionTimeOut())
-                .readTimeOut(readTimeOut())
-                .writeTimeOut(writeTimeOut())
-                .pre(getPre())
-                .post(getPost())
-                .dir(getDir())
-                .size(getSize())
-                .debug(isDebug())
+                .url(eqConfig.getUrl())
+                .connectTimeOut(eqConfig.connectionTimeOut())
+                .debug(eqConfig.isDebug())
+                .dir(eqConfig.getDir(this))
+                .post(eqConfig.getPost())
+                .pre(eqConfig.getPre())
+                .readTimeOut(eqConfig.readTimeOut())
                 .build();
         imgLoader = PicassoLoader.with(this);
     }
@@ -50,69 +52,4 @@ public abstract class EQApplication extends Application {
         return context;
     }
 
-
-    /**
-     * 是否开启调试模式
-     *
-     * @return
-     */
-    public abstract boolean isDebug();
-
-    /**
-     * 配置服务器地址
-     *
-     * @return
-     */
-    public abstract String getUrl();
-
-    /**
-     * 请求之前的拦截器
-     *
-     * @return
-     */
-    public List<Interceptor> getPre() {
-        return Collections.emptyList();
-    }
-
-    /**
-     * 请求之后的拦截器
-     *
-     * @return
-     */
-    public List<Interceptor> getPost() {
-        return Collections.emptyList();
-    }
-
-    /**
-     * 默认的缓存目录 eq_cache
-     *
-     * @return
-     */
-    public String getDir() {
-        String parent = SdCardUtils.getDiskCacheDirPath(this);
-        File dir = FileUtils.createDir(parent, "eq_cache");
-        return dir.getPath();
-    }
-
-    /**
-     * 配置缓存大小,默认是10M
-     *
-     * @return
-     */
-    public int getSize() {
-        return 10;
-    }
-
-    //10秒钟
-    public int connectionTimeOut() {
-        return 10;
-    }
-
-    public int readTimeOut() {
-        return 10;
-    }
-
-    public int writeTimeOut() {
-        return 10;
-    }
 }
